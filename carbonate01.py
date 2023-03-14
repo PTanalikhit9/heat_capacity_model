5240b4701ae94c1c600824a2c95d2fa591e14987
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-14 20:21:27 +0700

    Update carbonate01.py

--- carbonate01.py
+++ carbonate01.py
@@ -16,3 +16,30 @@ Q = 200
 
 temp = []
 Cp_values = []
+
+for i in range(len(t)):
+    T = carbonate_water_temperature(t[i], T0, Cp0, mc, Q, dt)
+    temp.append(T)
+    Cp_values.append(Cp0 * np.exp(-0.01 * T))
+    T0 = T
+
+temp = np.array(temp)
+Cp_values = np.array(Cp_values)
+
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+axs[0].plot(t, temp)
+axs[0].set_xlabel('Time (s)')
+axs[0].set_ylabel('Temperature (C)')
+axs[0].set_title('Temperature vs Time')
+
+axs[1].plot(temp, Cp_values)
+axs[1].set_xlabel('Temperature (C)')
+axs[1].set_ylabel('Specific Heat Capacity (J/kg.C)')
+axs[1].set_title('Specific Heat Capacity vs Temperature')
+
+axs[2].plot(t, Cp_values)
+axs[2].set_xlabel('Time (s)')
+axs[2].set_ylabel('Specific Heat Capacity (J/kg.C)')
+axs[2].set_title('Specific Heat Capacity vs Time')
+
+plt.show()
