d64e9b891edb07bc80b1958ee2ec25e1836cb9f4
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-14 20:20:35 +0700

    Update carbonate01.py

--- carbonate01.py
+++ carbonate01.py
@@ -6,3 +6,13 @@ def carbonate_water_temperature(t, T, Cp0, mc, Q, dt):
     h = 0.1 * np.exp(-0.05 * t) # Modeling the time dependence of heat transfer coefficient
     dTdt = (Q - h * (T - 20)) / (mc * Cp)
     return T + dTdt * dt
+
+dt = 0.1
+t = np.linspace(0, 100, 1000)
+T0 = 20
+Cp0 = 4.18
+mc = 0.5
+Q = 200
+
+temp = []
+Cp_values = []
