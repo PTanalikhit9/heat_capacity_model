66752f6536d6575458e54a3794216d43a5ee87da
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-15 20:29:17 +0700

    Update water1.py

--- water1.py
+++ water1.py
@@ -19,3 +19,35 @@ def heat_capacity(T):
 def temperature_change(energy, mass, T):
     return energy / (mass * heat_capacity(T))
 
+# Initialize lists to store time, temperature, and heat capacity data
+time_data = [0]
+temperature_data = [initial_temperature]
+heat_capacity_data = [heat_capacity(initial_temperature)]
+
+# Simulate heating process
+current_temperature = initial_temperature
+while current_temperature < final_temperature:
+    energy_input = heater_power * time_step
+    delta_temperature = temperature_change(energy_input, mass, current_temperature)
+    current_temperature += delta_temperature
+
+    time_data.append(time_data[-1] + time_step)
+    temperature_data.append(current_temperature)
+    heat_capacity_data.append(heat_capacity(current_temperature))
+
+# Plot the results
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
+
+ax1.plot(time_data, temperature_data)
+ax1.set_xlabel("Time (s)")
+ax1.set_ylabel("Temperature (°C)")
+ax1.set_title("Temperature vs Time")
+
+ax2.plot(temperature_data, heat_capacity_data)
+ax2.set_xlabel("Temperature (°C)")
+ax2.set_ylabel("Specific Heat Capacity (J/kg·K)")
+ax2.set_title("Specific Heat Capacity vs Temperature")
+
+plt.tight_layout()
+plt.show()
+
